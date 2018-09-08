ba7d24ebe1702737a850a796d43b8e0db0a96c51
Janhavi Dahihande <janhavidahihande@gmail.com>
2018-09-08 01:30:38 -0700

    Added functionality for 3 machines

--- lab1/gumball-java-pattern/Main.java
+++ lab1/gumball-java-pattern/Main.java
@@ -1,22 +1,23 @@
 
-
+import java.util.*;
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
 
-		System.out.println(gumballMachine);
+        System.out.println(gumballMachine);
 
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
+        gumballMachine.insertCoins(Arrays.asList(25));
+        gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+        System.out.println(gumballMachine);
 
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
+        gumballMachine.insertCoins(Arrays.asList(25,25));
+        gumballMachine.turnCrank();
+        
+        gumballMachine.insertCoins(Arrays.asList(25,10,10,5));
+        gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
-	}
+        System.out.println(gumballMachine);
+    }
 }
