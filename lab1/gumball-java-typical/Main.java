86f591f1810dc4920db318ed242f196a912c97d7
Janhavi Dahihande <janhavidahihande@gmail.com>
2018-09-08 01:19:35 -0700

    Added functionality for 3 machines

--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -9,12 +9,15 @@ public class Main {
 
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
+		
+		gumballMachine.insertQuarter( 25 );
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
+		
 		gumballMachine.insertQuarter( 25 );
+		gumballMachine.insertQuarter( 10 );
+		gumballMachine.insertQuarter( 10 );
+		gumballMachine.insertQuarter( 5 );
 		gumballMachine.turnCrank();
 
 		System.out.println(gumballMachine);
